9e2b920a8a18580a29d0d0fd2d4a5a3f8c0613ff
papapapvf <ptyz2000@gmail.com>
2016-08-03 20:04:48 +0300

    e

--- client/camera.py
+++ client/camera.py
@@ -1,2 +1,12 @@
+<<<<<<< HEAD
+import cv
+capture = cv.CaptureFromCAM(0)
+img = cv.QueryFrame(capture)
+cv.SaveImage("img/parking.jpg", img)
+
+def get_image(cam_id):
+	return (open('img/parkng.jpg', 'rb'), open('img/background.jpg', 'rb'))
+=======
 def get_image(cam_id):
 	return (open('img/parking.jpg', 'rb'), open('img/background.jpg', 'rb'))
+>>>>>>> 35284c370f7c53f80b2696afa4634a30b0f8585b
